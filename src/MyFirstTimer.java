b6d1f7b09959019f680e7258e86947ff3d4385a5
Hunter <h_kruse@u.pacifc.edu>
2025-02-19 16:13:41 -0800

    added Timer, actionPerformed(), start() and end()

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,11 +1,16 @@
+import java.awt.event.*;
+import javax.swing.*;
+
 import acm.graphics.*;
 import acm.program.*;
 
-public class MyFirstTimer extends GraphicsProgram {
+public class MyFirstTimer extends GraphicsProgram implements ActionListener{
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private GLabel myLabel;
+	private int count;
+	private Timer t;
 
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -13,8 +18,24 @@ public class MyFirstTimer extends GraphicsProgram {
 	}
 	
 	public void run() {
-		myLabel = new GLabel("# of times called?", 0, 100);
+		count = 0;
+		myLabel = new GLabel("# of times called? " + count, 0, 100);
 		add(myLabel);
+		
+		t = new Timer(1000, this);
+		t.start();
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		myLabel.move(0, 5);
+		count++;
+		
+		myLabel.setLabel("# of times called? " + count);
+		
+		if(count >= 10) {
+			t.stop();
+		}
 	}
 	
 	public static void main(String[] args) {
